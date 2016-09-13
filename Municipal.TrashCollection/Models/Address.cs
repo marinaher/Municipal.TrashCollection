a6ef41c33ffe34970bbeb25a198aaca02d2be46b
marinaher <b-her@live.com>
2016-09-13 17:24:54 -0500

    Created Address Class

---
+++ Municipal.TrashCollection/Models/Address.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Municipal.TrashCollection.Models
+{
+    public class Address
+    {
+        [Key]
+        public int ID { get; set; }
+        public string ApartmentNumber { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+    }
+}
